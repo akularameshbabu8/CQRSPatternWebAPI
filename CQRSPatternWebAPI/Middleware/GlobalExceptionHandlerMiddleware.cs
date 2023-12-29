59b052015ff8db27cf23e69ede48de8a76732d4a
Rameshbabu Akula <jjjj>
2023-12-29 16:47:51 +0530

    Committed Changes

--- CQRSPatternWebAPI/Middleware/GlobalExceptionHandlerMiddleware.cs
+++ CQRSPatternWebAPI/Middleware/GlobalExceptionHandlerMiddleware.cs
@@ -9,19 +9,18 @@ namespace CQRSPatternWebAPI.Middleware
     public class GlobalExceptionMiddleware
     {
         private readonly RequestDelegate _next;
+
         public GlobalExceptionMiddleware(RequestDelegate next)
         {
-
             _next = next;
         }
 
         public async Task InvokeAsync(HttpContext context)
         {
-
             try
             {
                 await _next(context);
-            }           
+            }
             catch (Exception ex)
             {
                 await HandleExceptionAsync(context, ex);
@@ -30,17 +29,17 @@ namespace CQRSPatternWebAPI.Middleware
 
         private async Task HandleExceptionAsync(HttpContext context, Exception ex)
         {
-            context.Response.ContentType = MediaTypeNames.Application.Json;
-            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-
-            string message = string.Empty;
-            if (ex.GetType() == typeof(Exception))
+            if (!context.Response.HasStarted)
             {
-                message = "Internal Server error.";
-            }            
+                context.Response.ContentType = MediaTypeNames.Application.Json;
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
-            byte[] error = Encoding.ASCII.GetBytes(new ErrorDetails() { StatusCode = context.Response.StatusCode, Message = message }.ToString());
-            await context.Response.Body.WriteAsync(error, 0, error.Length);
+                string message = "Internal Server error.";
+
+                byte[] error = Encoding.ASCII.GetBytes(new ErrorDetails() { StatusCode = context.Response.StatusCode, Message = message }.ToString());
+                await context.Response.Body.WriteAsync(error, 0, error.Length);
+            }
         }
     }
+
 }
